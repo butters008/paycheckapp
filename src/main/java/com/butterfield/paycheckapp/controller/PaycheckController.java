6935ffc17407ca56694a632a3602bc726432a7e2
Butters <k.butterfield08@gmail.com>
2023-03-07 17:10:09 -0600

    Got the Create Done

--- src/main/java/com/butterfield/paycheckapp/controller/PaycheckController.java
+++ src/main/java/com/butterfield/paycheckapp/controller/PaycheckController.java
@@ -2,15 +2,15 @@ package com.butterfield.paycheckapp.controller;
 
 import com.butterfield.paycheckapp.database.dao.PaycheckDAO;
 import com.butterfield.paycheckapp.database.entity.Paycheck;
+import com.butterfield.paycheckapp.formBean.PaycheckFormBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
 import java.util.List;
-
-import static java.lang.System.out;
 
 @Controller
 public class PaycheckController {
@@ -31,9 +31,30 @@ public class PaycheckController {
 
         List<Paycheck> paychecks = paycheckDAO.findAll();
         System.out.print(paychecks.stream().toList());
-        response.addObject(paychecks);
+        response.addObject("paycheckList", paychecks);
 
-        response.setViewName("paycheck/list");
+        response.setViewName("paycheckList");
+        return response;
+    }
+
+    @RequestMapping(value="paycheck/saveCheck", method = RequestMethod.POST)
+    public ModelAndView saveCheck(@Valid PaycheckFormBean form) throws Exception {
+        ModelAndView response = new ModelAndView();
+
+        //Adding Check
+        Paycheck paycheck = new Paycheck();
+        paycheck.setPaycheckName(form.getPaycheckName());
+        paycheck.setPaycheckAmount(form.getPaycheckAmount());
+        paycheck.setPaycheckDate(form.getPaycheckDate());
+        paycheck.setDayName(form.getDayName());
+        paycheck.setDaysBetweenCheck(form.getDaysBetweenCheck());
+
+        paycheckDAO.save(paycheck);
+//        response.addObject(paycheck);
+//        response.setViewName("redirect:../index/" + paycheck.getId());
+        response.setViewName("redirect:../paycheck/list");
+//        indexWithPaycheck();
+
         return response;
     }
 }
---
+++ src/main/java/com/butterfield/paycheckapp/formBean/PaycheckFormBean.java
@@ -0,0 +1,27 @@
+package com.butterfield.paycheckapp.formBean;
+
+
+import jakarta.persistence.Column;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@ToString
+public class PaycheckFormBean {
+    private Integer id;
+
+    //    @NotEmpty(message = "Cannot be blank")
+    private String paycheckName;
+
+    private Integer paycheckAmount;
+
+    private LocalDate paycheckDate;
+
+    private String dayName;
+
+    private Integer daysBetweenCheck;
+}
