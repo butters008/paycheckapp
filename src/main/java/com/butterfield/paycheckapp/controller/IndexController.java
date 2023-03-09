8c32d4231fc60b1510b52e42ba6ccdd69facd8c5
Butters <k.butterfield08@gmail.com>
2023-03-09 14:27:34 -0600

    Got Update (crUd) working for paycheck

--- src/main/java/com/butterfield/paycheckapp/controller/IndexController.java
+++ src/main/java/com/butterfield/paycheckapp/controller/IndexController.java
@@ -2,7 +2,6 @@ package com.butterfield.paycheckapp.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -14,13 +13,6 @@ public class IndexController {
     @RequestMapping(value = "/index", method = RequestMethod.GET)
     public ModelAndView index() throws Exception {
         ModelAndView response = new ModelAndView();
-        return response;
-    }
-
-    @RequestMapping(value = "/index/{aID}", method = {RequestMethod.GET, RequestMethod.POST})
-    public ModelAndView indexWithPaycheck(@PathVariable("aID") Integer id) throws Exception {
-        ModelAndView response = new ModelAndView();
-        log.info("Inside Paycheck Method " + id);
         return response;
     }
 }
--- src/main/java/com/butterfield/paycheckapp/database/dao/PaycheckDAO.java
+++ src/main/java/com/butterfield/paycheckapp/database/dao/PaycheckDAO.java
@@ -2,8 +2,10 @@ package com.butterfield.paycheckapp.database.dao;
 
 import com.butterfield.paycheckapp.database.entity.Paycheck;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PaycheckDAO extends JpaRepository<Paycheck, Long>  {
+    Paycheck findById(@Param("id") Integer id);
 }
